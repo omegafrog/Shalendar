ed2753eacc454d39db2943f8cd4471931750196f
omegafrog <jiwoo22110103@gmail.com>
2023-01-20 23:45:37 +0900

    Connect calendar api with db

--- src/calendar/controller.js
+++ src/calendar/controller.js
@@ -1,119 +1,135 @@
-const jwt = require('jsonwebtoken');
-const calendarRepository = require('./query');
-const calendarUserConnectionRepository = require('../data/interTableConnectionQuery');
-const userRepository = require('../user/query');
+const jwt = require("jsonwebtoken");
+const calendarRepository = require("./query");
+const userRepository = require("../user/query");
 
-exports.create = ctx=>{
-    let {name, theme} = ctx.request.body;
-    let {token} = ctx.request.header;
-    console.log(name, theme, token);
-    let userId;
-    jwt.verify(token, process.env.JWT_SECRET,(e,d)=>{
-        if(e){
-            ctx.response.status =400;
-            ctx.body={
-                result:"잘못된 유저 토큰입니다"
-            }
-            return;
-        }else{
-            userId = d.id;
-        }
-    });
+exports.create = async (ctx) => {
+  let { name } = ctx.request.body;
+  let { token } = ctx.request.header;
 
-    let user = userRepository.findByUserId(userId);
-    if(user == null){
-        ctx.response.status=400;
-        ctx.body={
-            result:"잘못된 유저 토큰입니다."
-        }
-        return;
-    }
+  if (token == undefined) {
+    ctx.response.status = 400;
+    ctx.body = {
+      result: "잘못된 유저 토큰입니다",
+    };
+  }
+  console.log(token);
+  // token에서 userId 가져옴
+  let userId;
+  userId = verifyToken(token, ctx);
 
-    let {affectedRows, calendarId} = calendarRepository.save(name, theme);
-    console.log('calendar save : '+calendarId)
-    if(affectedRows<0){
-        ctx.response.status = 400;
-        ctx.body = {
-            result:"Create Calendar failed"
-        }
-    }else{
-        affectedRows = calendarUserConnectionRepository.saveCalendarUserConnection(userId,calendarId).affectedRows;
+  let user = await userRepository.findByUserId(userId);
+  if (user == null) {
+    ctx.response.status = 400;
+    ctx.body = {
+      result: "잘못된 유저 토큰입니다.",
+    };
+    return;
+  }
 
-        if(affectedRows<0){
-            ctx.response.status=500;
-            ctx.body={
-                result:"캘린더와 유저 연결에 실패했습니다."
-            }
-            return;
-        }else{
-            ctx.body={
-                result:"ok",
-                insertId:calendarId
-            }
-        }
-    }
-}
-exports.delete = ctx=>{
-    let id = ctx.params.id;
-    let result = calendarRepository.findOne(id);
-    if(result == null){
-        ctx.response.status=400;
-        ctx.body={
-            result:"id가 "+id+"인 캘린더는 없습니다."
-        }
-        return;
-    }
-    let {affectedRows} = calendarRepository.remove(id);
-    if(affectedRows<0){
-        ctx.response.status=500;
-        ctx.body={
-            result:"Delete calendar failed"
-        }
-    }else{
-        ctx.body={
-            result:"ok"
-        }
-    }
-}
-exports.find = ctx=>{
-    let calendarId = ctx.query.calendarid;
-    let userid = ctx.query.userid;
-    if(calendarId!=null){
-        let result = calendarRepository.findOne(calendarId);
-        console.log(result);
-        if(result == null){
-            ctx.response.status=400;
-            ctx.body={
-                result:"id가 "+calendarId+"인 캘린더는 없습니다."
-            }
-        }else{
-            ctx.body={
-                calendar:result,
-                result:"ok" 
-            }
-        }
-    }
-    if(userid!=null){
-        let userId = ctx.query.userid;
-        let result = calendarUserConnectionRepository.findCalendarByUserId(userId);
-        if(result == null){
-            ctx.response.status=400;
-            ctx.body={
-                result:userId+'인 유저가 속한 캘린더는 없습니다.'
-            }
-        }else{
-            let calendarIdList=new Set();
-            result.forEach(
-                connection => calendarIdList.add(connection.calendarId)
-            );
-            console.log(calendarIdList);
-            let calendars = calendarRepository.findAllById(calendarIdList);
+  let { affectedRows, insertId } = await calendarRepository.save(name, userId);
 
-            ctx.body={
-                result:"ok",
-                calendars:calendars
-            }
-        }
+  if (affectedRows < 0) {
+    ctx.response.status = 400;
+    ctx.body = {
+      result: "Create Calendar failed",
+    };
+  } else {
+    affectedRows = await calendarRepository.connect(insertId, userId)
+      .affectedRows;
+    if (affectedRows < 0) {
+      ctx.response.status = 500;
+      ctx.body = {
+        result: "Connect Calendar failed",
+      };
+    } else {
+      ctx.body = {
+        result: "ok",
+        insertId: insertId,
+      };
     }
-    
+  }
+};
+exports.delete = async (ctx) => {
+  let id = ctx.params.id;
+  let result = await calendarRepository.findById(id);
+  if (result == null) {
+    ctx.response.status = 400;
+    ctx.body = {
+      result: "id가 " + id + "인 캘린더는 없습니다.",
+    };
+    return;
+  }
+  let { affectedRows } = await calendarRepository.disconectByCalendarId(id);
+  if (affectedRows < 0) {
+    ctx.response.status = 500;
+    ctx.body = {
+      result: "Disconnect between calendar and user failed",
+    };
+    return;
+  }
+  affectedRows = await calendarRepository.remove(id).affectedRows;
+  if (affectedRows < 0) {
+    ctx.response.status = 500;
+    ctx.body = {
+      result: "Delete calendar failed",
+    };
+  } else {
+    ctx.body = {
+      result: "ok",
+    };
+  }
+};
+exports.findById = async (ctx) => {
+  let calendarId = ctx.params.id;
+  if (calendarId != null) {
+    let result = await calendarRepository.findById(calendarId);
+    console.log(result);
+    if (result == null) {
+      ctx.response.status = 400;
+      ctx.body = {
+        result: "id가 " + calendarId + "인 캘린더는 없습니다.",
+      };
+    } else {
+      ctx.body = {
+        calendar: result,
+        result: "ok",
+      };
+    }
+  } else {
+    ctx.response.status = 400;
+    ctx.body = {
+      result: "Wrong request",
+    };
+  }
+};
+
+exports.findByUserId = async (ctx) => {
+  let userId = verifyToken(ctx.request.header.token, ctx);
+  if (userId != null) {
+    let result = await calendarRepository.findByUserId(userId);
+    if (result == null) {
+      ctx.response.status = 400;
+      ctx.body = {
+        result: userId + "인 유저가 속한 캘린더는 없습니다.",
+      };
+    } else {
+      ctx.body = {
+        result: "ok",
+        calendars: result,
+      };
+    }
+  }
+};
+function verifyToken(token, ctx) {
+  let userId;
+  jwt.verify(token, process.env.JWT_SECRET, (e, d) => {
+    if (e) {
+      ctx.response.status = 400;
+      ctx.body = {
+        result: "잘못된 유저 토큰입니다",
+      };
+    }
+    userId = d.id;
+  });
+  return userId;
 }
--- src/calendar/query.js
+++ src/calendar/query.js
@@ -1,25 +1,14 @@
-
+const {pool} = require('../data/connection');
 let calendars=[];
 let sequence = 0;
 
-exports.save=(name, theme, userId)=>{
-    let newCalendar={
-        id:sequence++,
-        name:name,
-        theme:theme,
-        userId:userId
-    }
-    calendars.push(newCalendar);
-    let result = {
-        affectedRows:1,
-        calendarId:newCalendar.id
-    }
-    return result
+exports.save=async (name, userId)=>{
+    const query = `INSERT INTO calendar (calendar_name) values (?)`;
+    return await pool(query, [name, userId]);
 }
-exports.findOne=(id)=>{
-    let founded = calendars.filter(
-        calendar => calendar.id==id
-    )
+exports.findById=async (id)=>{
+    const query = `SELECT * FROM calendar WHERE calendar_id = ?`;
+    let founded = await pool(query, [id]);
     return (founded.length==0)?null:founded[0];
 }
 exports.findAllById=(idSet)=>{
@@ -30,19 +19,25 @@ exports.findAllById=(idSet)=>{
     return result;
 }
 
-exports.findByUserId=(userId)=>{
-    let founded = calendars.filter(
-        calendar=>calendar.userId == userId
-    )
-    return (founded.length()==0)?null:founded;
+exports.findByUserId= async (calendarId, userId)=>{
+    const query = `SELECT * FROM calendar 
+    INNER JOIN calendar_user_connection as connection
+    ON connection.calendar_conn_id = calendar.calendar_id
+    WHERE connection.user_conn_id = ?`;
+    let founded = await pool(query, [calendarId, userId]);
+    return (founded.length==0)?null:founded;
 }
-exports.remove=(id)=>{
-    let affectedRows = 0;
-    calendars = calendars.filter(
-        calendar=>{calendar.id !=id}
-    )
-    let res={
-        affectedRows:affectedRows
-    }
-    return res;
+exports.remove=async (id)=>{
+    const query = `DELETE FROM calendar WHERE calendar_id=?`;
+    await pool(query, [id]);
+}
+
+exports.connect = async (calendarId, userId)=>{
+    const query = `INSERT INTO calendar_user_connection (calendar_conn_id, user_conn_id)
+    values(?,?)`;
+    return await pool(query, [calendarId, userId]); 
+}
+exports.disconectByCalendarId = async(calendarId)=>{
+    const query = `DELETE FROM calendar_user_connection WHERE calendar_conn_id=?`;
+    return await pool(query, [calendarId]);
 }
