739d0ae44d6b3032a842b8f324fe60759fd1e129
omegafrog <jiwoo22110103@gmail.com>
2023-01-22 02:21:26 +0900

    Refactor todo api to use db

--- src/todo/controller.js
+++ src/todo/controller.js
@@ -1,174 +1,142 @@
+const todoRepository = require("./query");
 
-const todoRepository = require('./query');
-const jwt = require('jsonwebtoken');
-
-
-exports.create= ctx=>{
-    let {title, content} = ctx.request.body;
-    let token = ctx.request.header;
-
-    // get userId from token's payload
-    let userId;
-    jwt.verify(token, process.env.JWT_SECRET, (e,d)=>{
-        if(e){
-            ctx.response.status = 400;
-            ctx.body={
-                result:"유저 인증 실패"
-            }
-        }else{
-            userId = d.id;        
-        }
-    });
-
-    let calendarId = ctx.params.id;
-
-    // save new todo into db
-    let {affectedRows, insertId} = todoRepository.save(title, content, calendarId);
-
-
-    if(affectedRows<0){
-        ctx.response.status=400;
-        ctx.body = {
-            result:"Save Todo failed"
-        }
-    }else{
-        ctx.body={
-            result:"ok",
-            insertId:insertId
-        }
+exports.create = async (ctx) => {
+  let { title } = ctx.request.body;
+  if(title == undefined){
+    ctx.response.status = 400;
+    ctx.body = {
+      result : "Title is undefined"
     }
-}
+  }
 
+  let calendarId = ctx.params.id;
 
-exports.find= ctx=>{
-    let todoId = ctx.params.id;
-    let result = todoRepository.findById(todoId);
-    if(result !=null){
-        ctx.body={
-            result:"ok",
-            todo:result
-        }
-    }else{
-        ctx.response.status=400;
-        ctx.body={
-            result:"Failed to find todo"
-        }
-    }
+  // save new todo into db
+  let { affectedRows, insertId } = await todoRepository.save(title, calendarId);
+  if (affectedRows < 0) {
+    ctx.response.status = 400;
+    ctx.body = {
+      result: "Save Todo failed",
+    };
+  } else {
+    ctx.body = {
+      result: "ok",
+      insertId: insertId,
+    };
+  }
+};
 
-}
-exports.findByCalendarId= ctx=>{
-    let calendarId = ctx.params.id;
-    let result = todoRepository.findByCalendarId(calendarId);
-    if(result!=null){
-        ctx.body={
-            result:"ok",
-            todos:result
-        }
-    }else{
-        ctx.response.status=400;
-        ctx.body={
-            result:"Failed to find todo by calendar Id"
-        }
+exports.find = async (ctx) => {
+  let todoId = ctx.params.id;
+  let result = await todoRepository.findById(todoId);
+  if (result != null) {
+    ctx.body = {
+      result: "ok",
+      todo: result,
+    };
+  } else {
+    ctx.response.status = 400;
+    ctx.body = {
+      result: "Failed to find todo",
+    };
+  }
+};
+exports.findByCalendarId = async (ctx) => {
+  let calendarId = ctx.params.id;
+  let result = await todoRepository.findByCalendarId(calendarId);
+  if (result != null) {
+    ctx.body = {
+      result: "ok",
+      todos: result,
+    };
+  } else {
+    ctx.response.status = 400;
+    ctx.body = {
+      result: "Failed to find todo by calendar Id",
+    };
+  }
+};
+exports.findByUserId = async (ctx) => {
+  let {userId} = ctx.state;
+  if (userId == null) {
+    ctx.response.status = 400;
+    ctx.body = {
+      result: "로그인이 필요합니다.",
+    };
+  } else {
+    let foundedTodos = await todoRepository.findAllByUserId(userId);
+    if (foundedTodos != null) {
+      ctx.body = {
+        result: "ok",
+        todos: foundedTodos,
+      };
+    } else {
+      ctx.response.status = 400;
+      ctx.body = {
+        result: "Failed to find todo",
+      };
     }
-}
-exports.findByUserId= ctx=>{
-    let {token} = ctx.request.header;
-    let userId;
-    if(token == null){
-        ctx.response.status=400;
-        ctx.body={
-            result:"로그인이 필요합니다."
-        }
-    }else{
-        jwt.verify(token, process.env.JWT_SECRET, (e,d)=>{
-            if(e){
-                ctx.response.status=400;
-                ctx.body={
-                    result:"Failed to login",
-                }
-            }else{
-               userId= d.id;
-            }
-        });
-        
-        let foundedTodos;
-        if(result!=null){
-            foundedTodos = todoRepository.findAllByUserId(userId);
-        }
-        if(foundedTodos!=null){
-            ctx.body = {
-                result:"ok",
-                todos:foundedTodos
-            }
-        }else{
-            ctx.response.status=400;
-            ctx.body={
-                result:"Failed to find todo",
-            }
-        }
+  }
+};
+exports.delete = async (ctx) => {
+  let id = ctx.params.id;
+  
+  let { affectedRows } = todoRepository.remove(id);
+  if (affectedRows < 0) {
+    ctx.response.status = 400;
+    ctx.body = {
+      result: "Failed to delete todo",
+    };
+  } else {
+    ctx.body = {
+      result: "ok",
+    };
+  }
+};
+exports.update = async (ctx) => {
+  let id = ctx.params.id;
+  let { title } = ctx.request.body;
+  let result =await todoRepository.findById(id);
+  if (result != null) {
+    let updatedTodo = {
+      todo_id: id,
+      title: title,
+      isComplete: result.isComplete,
+      calendar_id: result.calendar_id,
+    };
+    let { affectedRows } = await todoRepository.update(id, updatedTodo);
+    if (affectedRows < 0) {
+      ctx.response.status = 400;
+      ctx.body = {
+        result: "Failed to update todo",
+      };
+    } else {
+      ctx.body = {
+        result: "ok",
+        insertId: id,
+      };
     }
-}
-exports.delete= ctx=>{
-    let id = ctx.params.id;
-    let {affectedRows} = todoRepository.remove(id);
-    if(affectedRows<0){
-        ctx.response.status=400;
-        ctx.body={
-            result:"Failed to delete todo"
-        }
-    }else{
-        affectedRows = todoUserConnectionRepository.removeByTodoId(id);
-        if(affectedRows<0){
-            ctx.response.status=400;
-            ctx.body={
-                result:"Failed to remove connection between todo and user"
-            }   
-        }
-        ctx.body={
-            result:"ok"
-        }
+  }else{
+    ctx.response.status=400;
+    ctx.body={
+        result:"Failed to find todo"
     }
-}
-exports.update= ctx=>{
-    let id = ctx.params.id;
-    let {title, content} = ctx.request.body;
-    let result = todoRepository.findById(id);
-    if(result!=null){
-        let updatedTodo = {
-            id:id,
-            title:title,
-            content:content,
-            isComplete:result.isComplete,
-            calendarId:result.calendarId
-        }
-        let {affectedRows} = todoRepository.update(id, updatedTodo);
-        if(affectedRows<0){
-            ctx.response.status=400;
-            ctx.body={
-                result:"Failed to update todo"
-            }
-        }else{
-            ctx.body={
-                result:"ok",
-                insertId: id
-            }
-        }
-    }
-}
-exports.checkComplete= ctx=>{
-    let id = ctx.params.id;
-    let result = todoRepository.findById(id);
-    result.isComplete = true;
-    let {affectedRows} = todoRepository.update(id, result);
-    if(affectedRows<0){
-        ctx.response.status=400;
-        ctx.body={
-            result:"Failed to check complete"
-        }
-    }else{
-        ctx.body={
-            result:"ok",
-            checkedId:id
-        }
-    }
-}
+  }
+};
+exports.checkComplete = async (ctx) => {
+  let id = ctx.params.id;
+  let result = await todoRepository.findById(id);
+  result.isComplete = true;
+  let { affectedRows } = todoRepository.update(id, result);
+  if (affectedRows < 0) {
+    ctx.response.status = 400;
+    ctx.body = {
+      result: "Failed to check complete",
+    };
+  } else {
+    ctx.body = {
+      result: "ok",
+      checkedId: id,
+    };
+  }
+};
--- src/todo/query.js
+++ src/todo/query.js
@@ -1,38 +1,35 @@
-let todos=[];
-let sequence=0;
+const { pool } = require("../data/connection");
 
-exports.save=(title, content, calendarId)=>{
-    let newTodo = {
-        id:sequence++,
-        title:title,
-        content:content,
-        calendarId:calendarId,
-        isComplete:false
-    }
-    todos.push(newTodo);
-    let res={
-        affectedRows:1,
-        insertId:newTodo.id
-    }
-    return res;
-}
-exports.findById=(id)=>{
-    let founded = todos.find( todo => todo.id == id);
-    return (founded.length==0)?null:founded[0];
-}
-exports.findByCalendarId=(calendarId)=>{
-    let founded = todos.find( todo => todo.calendarId==calendarId);
-    return (founded.length == 0)?null:founded;
-}
-exports.findAllByUserId=(userId)=>{
-    let founded = [];
-    return (founded.length == 0)?null:founded;
-}
-exports.remove=(id)=>{
-    let founded = todos.find( todo => todo.id == id);
-    
+let todos = [];
+let sequence = 0;
 
-}
-exports.update=()=>{
-
-}
+exports.save = async (title, calendarId) => {
+  const query = `INSERT INTO todo (title, calendar_id) values (?,?)`;
+  return await pool(query, [title, calendarId]);
+};
+exports.findById = async (id) => {
+  const query = `SELECT * FROM todo WHERE todo_id=?`;
+  let founded = await pool(query, [id]);
+  return founded.length == 0 ? null : founded[0];
+};
+exports.findByCalendarId = async (calendarId) => {
+  const query = `SELECT * FROM todo WHERE calendar_id=?`;
+  let founded = await pool(query, [calendarId]);
+  return founded.length == 0 ? null : founded;
+};
+exports.findAllByUserId = async (userId) => {
+  const query = `SELECT
+     todo_id, title, isComplete, calendar_id, created_at, user_conn_id
+     FROM todo INNER JOIN calendar_user_connection ON calendar_id = calendar_conn_id
+    WHERE user_conn_id=?`;
+  let founded = await pool(query, [userId]);
+  return founded.length == 0 ? null : founded;
+};
+exports.remove = async (id) => {
+  const query = `DELETE FROM todo WHERE todo_id = ?`;
+  return await pool(query, [id]);
+};
+exports.update = async (id, result) => {
+  const query = `UPDATE todo SET title = ?, isComplete=? WHERE todo_id = ?`;
+  return await pool(query, [result.title, result.isComplete, id]);
+};
